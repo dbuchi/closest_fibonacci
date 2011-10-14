ce8662da6898c417ab829e1f60678b97bd0179c4
deeptibu <deepti.buchi@gmail.com>
2011-10-14 12:32:32 -0700

    corrected test case

--- test/test_closest_fibonacci.rb
+++ test/test_closest_fibonacci.rb
@@ -3,10 +3,11 @@ require 'closest_fibonacci'
 
 class TestClosestFibonacci < Test::Unit::TestCase
   should "match the below values" do
-    assert(0, 0.closest_fibonacci)
-    assert(1, 1.closest_fibonacci)
-    assert(8, 12.closest_fibonacci)
-    assert(144, 156.closest_fibonacci)
-    assert(89, 99.closest_fibonacci)
+    assert_equal(0, 0.closest_fibonacci)
+    assert_equal(0, 1.closest_fibonacci)
+    assert_equal(8, 12.closest_fibonacci)
+    assert_equal(144, 156.closest_fibonacci)
+    assert_equal(89, 99.closest_fibonacci)
   end
 end
+
